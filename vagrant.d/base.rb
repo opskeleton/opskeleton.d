f5c788c8f61359cc30f87e860f0c6a318b128d13
ronen <narkisr@gmail.com>
2017-03-15 23:51:22 +1100

    adding skip defaults

--- vagrant.d/base.rb
+++ vagrant.d/base.rb
@@ -1,4 +1,6 @@
-require_relative 'mirror'
-require_relative 'provision'
-require_relative 'caching'
-require_relative 'libvirt'
+unless ENV['SKIP_DEFAULTS']
+  require_relative 'mirror'
+  require_relative 'provision'
+  require_relative 'caching'
+  require_relative 'libvirt'
+end
